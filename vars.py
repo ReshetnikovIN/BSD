f7bde30b9bfe7b9e53a3c0b5d5c30265ff6ad97e
ReshetnikovIN <mail@reshetnikov-in.com>
2024-09-06 09:00:41 +0300

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,11 @@
+# string program
+tasks = "12"
+hours = "1.5"
+course = "Python"
+tasks_hours = float(hours) / int(tasks)
+tasks_hours_ = str(tasks_hours)
+
+print("Курс: " + course + ", всего задач: " + tasks + ", затрачено часов: " + hours + ", среднее время выполнения " + tasks_hours_ + " часа.")
+
+
+
