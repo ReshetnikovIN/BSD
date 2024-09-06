c4fbd83d0fa5f221635597d2084126fdec8ffa37
ReshetnikovIN <mail@reshetnikov-in.com>
2024-09-06 10:17:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = tuple([1, 2, 'One', 'Two', True, False])
+print(immutable_var)
+# immutable_var[1] = 'Один'
+mutable_list = [11, 22, 'OneOne', 'TwoTwo   ', True, False]
+mutable_list[0] = 111
+mutable_list[3] = 'OneOneOne'
+print(mutable_list)
