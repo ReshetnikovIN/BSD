0d3c9603d5c50109ffe40ccc2fba11d147859daa
ReshetnikovIN <mail@reshetnikov-in.com>
2024-09-09 05:53:13 +0300

    Add files via upload

---
+++ module_3_1_.py
@@ -0,0 +1,37 @@
+def string_info(str_):
+    global calls
+    str_low = str_.lower()
+    str_upp = str_.upper()
+    str_len = len(str_)
+    tuple_01 = (str_len,) + (str_upp,) + (str_low,)
+    count_calls()
+    return tuple_01
+
+
+def is_contains(str_in, list_to_search):
+    global calls
+    str_low = str_in.lower()
+    TF = False
+    for id in list_to_search:
+        lst_low = id.lower()
+        if lst_low == str_low:
+            TF = True
+    count_calls()
+    return TF
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+
