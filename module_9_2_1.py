9cfdd7391e7df0366c561d8ca8871f5e1f638810
Reshetnikov Igor N. <mail@reshetnikov-in.com>
2024-10-08 08:48:46 +0300

    Add files via upload

---
+++ module_9_2_1.py
@@ -0,0 +1,12 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(st) for st in first_strings if len(st) > 5 ]
+
+second_result = [(st_1,) + (st_2,) for st_1 in first_strings for st_2 in second_strings if len(st_1) == len(st_2)]
+
+third_result  = {st: len(st) for st in (first_strings + second_strings) if not len(st) % 2 }
+
+print(first_result)
+print(second_result)
+print(third_result)
